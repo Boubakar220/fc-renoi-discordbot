bc37a7e9aa675c93dd50c3c3bc6a7b9619931070
boubakar <amor.dupa@outlook.fr>
2019-09-08 02:35:39 +0200

    Changement du bot

---
+++ Desktop/Discord bot/main.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+const { TOKEN, PREFIX} = require("./config");
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log("Je suis prêt !");
+});
+
+client.on('message', msg => {
+  if (msg.content === 'Qui est le plus beau ?') msg.channel.send('Amor biensur');
+});
+
+client.on('message', msg => {
+  if (msg.content.startsWith(`${PREFIX}ping`)) msg.channel.send("Pong!");
+});
+
+client.login(TOKEN);
